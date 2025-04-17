dffeb950e9fa9d5ef99904927103cffbb4fe8831
KGAdev-1 <greatadokaty@icloud.com>
2025-04-17 19:12:53 +0200

    Logo rotation and onboard2 implementation

---
+++ CityScout/OnBoard2ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  OnBoard2ViewController.swift
+//  CityScout
+//
+//  Created by Umuco Auca on 17/04/2025.
+//
+
+import UIKit
+
+class OnBoard2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
