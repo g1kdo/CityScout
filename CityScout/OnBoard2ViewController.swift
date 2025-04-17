375a7a16e5e03268ec1812fbfed98bfd425a7ef8
KGAdev-1 <greatadokaty@icloud.com>
2025-04-17 19:16:13 +0200

    Logo rotation and onboard2 implementation

--- CityScout/OnBoard2ViewController.swift
+++ CityScout/OnBoard2ViewController.swift
@@ -9,13 +9,32 @@ import UIKit
 
 class OnBoard2ViewController: UIViewController {
 
+    
+    @IBOutlet var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-    }
-    
+              imageView.image = UIImage(named: "OnBoard")
+              imageView.translatesAutoresizingMaskIntoConstraints = false
+             // titleLabel.translatesAutoresizingMaskIntoConstraints = false
 
+              // ImageView center constraints
+              NSLayoutConstraint.activate([
+                  imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                  imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+                  imageView.widthAnchor.constraint(equalToConstant: 300),
+                  imageView.heightAnchor.constraint(equalToConstant: 500),
+                  imageView.topAnchor.constraint(equalTo:
+                      view.safeAreaLayoutGuide.topAnchor, constant: -30)
+              ])
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        imageView.layer.cornerRadius = 20
+        imageView.translatesAutoresizingMaskIntoConstraints = true
+    }
     /*
     // MARK: - Navigation
 
