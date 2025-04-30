6519adb88dbe67f54f327c71685d1efc3f4e5c95
KGAdev-1 <greatadokaty@icloud.com>
2025-04-30 11:32:42 +0200

    Initial Commit

---
+++ CityScout/CityScoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  CityScoutApp.swift
+//  CityScout
+//
+//  Created by Umuco Auca on 30/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct CityScoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CityScoutTests/CityScoutTests.swift
@@ -0,0 +1,17 @@
+//
+//  CityScoutTests.swift
+//  CityScoutTests
+//
+//  Created by Umuco Auca on 30/04/2025.
+//
+
+import Testing
+@testable import CityScout
+
+struct CityScoutTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
